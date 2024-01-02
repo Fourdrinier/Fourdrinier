4b34d48d6a8534d10233e9bc9d5115dd76754e64
Ethan Brown <ewbrowntech@gmail.com>
2024-01-02 00:25:40 -0600

    Added models for mods and playlists

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Table, Column, String, JSON, ForeignKey
 from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -10,6 +11,15 @@ async def create_tables(engine: AsyncEngine):
         await conn.run_sync(Base.metadata.create_all)
 
 
+# Association table for the many-to-many relationship
+mod_playset_association = Table(
+    "mod_playset",
+    Base.metadata,
+    Column("mod_id", String, ForeignKey("mods.id")),
+    Column("playset_id", String, ForeignKey("playsets.id")),
+)
+
+
 class Server(Base):
     __tablename__ = "servers"
     id = Column(String, primary_key=True, index=True)
@@ -17,3 +27,36 @@ class Server(Base):
     distribution = Column(String, index=True, nullable=False)  # EX: "paper"
     mc_version = Column(String, index=True, nullable=False)  # EX: "1.16.5"
     build_version = Column(String, index=True, nullable=False)  # EX: "652"
+
+
+class Mod(Base):
+    __tablename__ = "mods"
+    id = Column(String, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)  # EX: "EssentialsX"
+    loader = Column(String, index=True, nullable=False)  # EX: "paper"
+    supported_versions = Column(JSON, nullable=False)  # EX: ["1.12.4", "1.19.2"]
+    versions = relationship("Version", back_populates="mod")
+    playsets = relationship(
+        "Playset", secondary=mod_playset_association, back_populates="mods"
+    )
+
+
+class Version(Base):
+    __tablename__ = "versions"
+    id = Column(String, primary_key=True, index=True)
+    version_number = Column(String, nullable=False)
+    game_version = Column(String, nullable=False)  # Added this field
+    dependencies = Column(
+        JSON, nullable=False
+    )  # EX: [{ mod_id: "9s6osm5g", dependency_type: "optional", required_version: "hFdJG9fY"}]
+    mod_id = Column(String, ForeignKey("mods.id"))
+    mod = relationship("Mod", back_populates="versions")
+
+
+class Playset(Base):
+    __tablename__ = "playsets"
+    id = Column(String, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    mods = relationship(
+        "Mod", secondary=mod_playset_association, back_populates="playsets"
+    )
