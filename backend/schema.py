0395ad4a7469ba1a4afd5ff73b56b6492ab34d88
Ethan Brown <ewbrowntech@gmail.com>
2024-01-02 14:42:12 -0600

    Modified playset creation schema to set default values for name and mods

--- backend/schema.py
+++ backend/schema.py
@@ -1,5 +1,8 @@
+from typing import List, Any
+
 from pydantic import BaseModel
 
 
 class PlaysetCreateSchema(BaseModel):
-    name: str
+    name: str = "My Playlist"  # Default name if not otherwise defined
+    mods: List[Any] = []  # Default to an empty list
