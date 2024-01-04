e149eaa841b1da729dcff3fefb95575704152b61
Ethan Brown <ewbrowntech@gmail.com>
2024-01-04 13:39:46 -0600

    Refactor build_server for readability

--- backend/packages/fabric/build_server.py
+++ backend/packages/fabric/build_server.py
@@ -28,62 +28,10 @@ async def build_server(server):
         "allocated_memory": server.allocated_memory,
     }
 
-    java_requirement = await get_java_requirement(server["game_version"])
-    loader_version = await get_loader_version(server["game_version"])
-    launcher_version = await get_launcher_version()
-    loader_url = f'https://meta.fabricmc.net/v2/versions/loader/{server["game_version"]}/{loader_version}/{launcher_version}/server/jar'
+    await build_dockerfile(server)
+    image_name = await build_docker_image(server)
 
-    # Set up the basics of the Dockerfile
-    dockerfile_content = [
-        f"FROM openjdk:{java_requirement}",
-        "WORKDIR /minecraft",
-        f'ADD "{loader_url}" /minecraft/fabric.jar',
-        f'RUN echo "eula={server["eula"]}" > /minecraft/eula.txt',
-        f'EXPOSE {server["port"]}',
-        f'CMD java -Xmx{server["allocated_memory"]}M -jar /minecraft/fabric.jar nogui',
-    ]
-
-    # Open the boilerplate Dockerfile
-    with open(
-        os.path.join(os.path.dirname(__file__), "boilerplate", "Dockerfile"), "r"
-    ) as boilerplate_dockerfile:
-        content = boilerplate_dockerfile.read()
-
-    # Tailor the boilerplate Dockerfile to the server
-    tailored_dockerfile = (
-        content.replace("${JAVA_VERSION}", str(java_requirement))
-        .replace("${GAME_VERSION}", server["game_version"])
-        .replace("${LOADER_URL}", loader_url)
-        .replace("${EULA}", str(server["eula"]).lower())
-        .replace("${PORT}", str(server["port"]))
-    )
-
-    # Get the path to the server's storage directory
-    server_directory = await get_server_directory(server["id"])
-    Path(server_directory).mkdir(parents=True, exist_ok=True)
-
-    with open(os.path.join(server_directory, "Dockerfile"), "w") as dockerfile:
-        dockerfile.write(tailored_dockerfile)
-
-    # Create a Docker client
-    client = docker.from_env()
-    image_name = "fabric-" + server["id"]
-    # The low-level API client gives more control and returns a generator
-    low_level_client = docker.APIClient(base_url="unix://var/run/docker.sock")
-    # Stream the build output
-    response = low_level_client.build(
-        path=server_directory, tag=image_name, decode=True
-    )
-    build_log = []
-    for item in response:
-        message = item.get("stream") or item.get("aux")
-        if message and message != "\n":
-            build_log.append(message)
-    if "Successfully built" in build_log[-2]:
-        return {"image_name": image_name}
-    else:
-        client.images.remove(image=image_name)
-        raise HTTPException(status_code=500, detail="Failed to build Docker image")
+    return {"image_name": image_name}
 
 
 async def get_java_requirement(game_version):
@@ -130,3 +78,60 @@ async def get_launcher_version():
         newest_stable_version = data[0]["version"]
 
     return newest_stable_version
+
+
+async def build_dockerfile(server):
+    java_requirement = await get_java_requirement(server["game_version"])
+    loader_version = await get_loader_version(server["game_version"])
+    launcher_version = await get_launcher_version()
+    api_url = "https://meta.fabricmc.net/v2/versions/loader"
+    loader_url = f'{api_url}/{server["game_version"]}/{loader_version}/{launcher_version}/server/jar'
+
+    # Open the boilerplate Dockerfile
+    with open(
+        os.path.join(os.path.dirname(__file__), "boilerplate", "Dockerfile"), "r"
+    ) as boilerplate_dockerfile:
+        content = boilerplate_dockerfile.read()
+
+    # Tailor the boilerplate Dockerfile to the server
+    tailored_dockerfile = (
+        content.replace("${JAVA_VERSION}", str(java_requirement))
+        .replace("${GAME_VERSION}", server["game_version"])
+        .replace("${LOADER_URL}", loader_url)
+        .replace("${EULA}", str(server["eula"]).lower())
+        .replace("${PORT}", str(server["port"]))
+    )
+
+    # Get the path to the server's storage directory
+    server_directory = await get_server_directory(server["id"])
+    Path(server_directory).mkdir(parents=True, exist_ok=True)
+
+    with open(os.path.join(server_directory, "Dockerfile"), "w") as dockerfile:
+        dockerfile.write(tailored_dockerfile)
+
+
+async def build_docker_image(server):
+    server_directory = await get_server_directory(server["id"])
+    image_name = "fabric-" + server["id"]
+
+    # Create a Docker client
+    with docker.APIClient(base_url="unix://var/run/docker.sock") as low_level_client:
+        # Stream the build output
+        response = low_level_client.build(
+            path=server_directory, tag=image_name, decode=True
+        )
+
+    # Compile the build log
+    build_log = []
+    for item in response:
+        message = item.get("stream") or item.get("aux")
+        if message and message != "\n":
+            build_log.append(message)
+
+    # If the build failed, remove the image. Otherwise, return the image name
+    if "Successfully built" in build_log[-2]:
+        return image_name
+    else:
+        with docker.from_env() as client:
+            client.images.remove(image=image_name)
+        raise HTTPException(status_code=500, detail="Failed to build Docker image")
