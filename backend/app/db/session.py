d9c7053dbc4d2ae0091f449d2e8b5ec3ae50456a
Ethan Brown <ethan@ewbrowntech.com>
2024-03-19 00:57:04 -0500

    Implement Alembic

---
+++ backend/app/db/session.py
@@ -0,0 +1,26 @@
+import asyncpg
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# Database connection URL
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@postgres:5432/postgres"
+
+# Create the SQLAlchemy async engine
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Create the session factory
+AsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+# Dependency to get a database session
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception:
+            raise
