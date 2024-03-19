3df6ad46d12d62127a596b323da38c74703eaf4f
Ethan Brown <ethan@ewbrowntech.com>
2024-03-19 00:18:28 -0500

    Reinitialize project

---
+++ backend/app/app.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
--- frontend/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <NuxtWelcome/>
-  </div>
-</template>
-<script setup lang="ts">
-</script>
