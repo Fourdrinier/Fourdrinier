60e27c5b8cd27d726c753efe81a53c50ff75db75
Ethan Brown <ethan@ewbrowntech.com>
2024-07-23 13:20:35 -0500

    Cleanup session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -10,28 +10,30 @@ All rights reserved. This file is part of the Fourdrinier project and is release
 the GPLv3 License. See the LICENSE file for more details.
 """
 
-import asyncpg
-from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
+from sqlalchemy.orm import DeclarativeBase
 
 
 # Database connection URL
 DATABASE_URL = "postgresql+asyncpg://postgres:postgres@postgres:5432/postgres"
 
-# Create the SQLAlchemy async engine
-engine = create_async_engine(DATABASE_URL, echo=True)
 
-# Create the session factory
-AsyncSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
+# Create a base class for models
+class Base(DeclarativeBase):
+    pass
+
+
+# Create the SQLAlchemy async session factory
+async_engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal: async_sessionmaker[AsyncSession] = async_sessionmaker(
+    autocommit=False, autoflush=False, bind=async_engine
 )
-
-Base = declarative_base()
 
 
 # Dependency to get a database session
-async def get_db():
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         try:
             yield session
