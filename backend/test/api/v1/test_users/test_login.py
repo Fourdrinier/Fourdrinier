4a040e73a8fad42411c8ca6c86e1f3a54f1a5b30
Ethan Brown <ethan@ewbrowntech.com>
2024-07-23 22:15:37 -0500

    Type hinting in test_login.py

--- backend/test/api/v1/test_users/test_login.py
+++ backend/test/api/v1/test_users/test_login.py
@@ -8,10 +8,12 @@ Test the functionality of POST /users/login
 
 import pytest
 
+from httpx import AsyncClient, Response
 import jwt
-import secrets
-from fastapi.testclient import TestClient
-from sqlalchemy import select
+from typing import Any, Tuple
+
+from sqlalchemy import Result, select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.app.db.models import User
 
@@ -20,53 +22,62 @@ from backend.app.dependencies.core.jwt.get_secret_key import get_secret_key
 
 @pytest.mark.asyncio
 async def test_login_000_nominal(
-    client,
-    test_db,
-    seed_user,
-    test_jwt_secret_key,
-    test_jwt_expiration_time,
-):
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+    test_jwt_secret_key: str,
+    test_jwt_expiration_time: str,
+) -> None:
     """
     Test 000 - Nominal
     Conditions: Correct username and password provided
     Result: HTTP 200 - JWT and refresh token returned
     """
     # Make the request
-    response = await client.post(
+    response: Response = await client.post(
         "/api/v1/users/login", json={"username": "test-user", "password": "password"}
     )
     assert response.status_code == 200
-    response_data = response.json()
+    response_data: Any = response.json()
 
     # Refresh the seeded user to get the refresh token
     await test_db.refresh(seed_user)
 
     # Verify the JWT
     assert "jwt" in response_data
-    token = response_data["jwt"]
+    token: Any = response_data["jwt"]
     assert token is not None
-    payload = jwt.decode(token, get_secret_key(), algorithms=["HS256"])
+    payload: Any = jwt.decode(  # type: ignore
+        token, get_secret_key(), algorithms=["HS256"]
+    )
     username: str = payload.get("sub")
     assert username == "test-user"
 
     # Verify the refresh token
     assert "refresh_token" in response_data
-    refresh_token = response_data["refresh_token"]
+    refresh_token: Any = response_data["refresh_token"]
     assert refresh_token is not None
     assert refresh_token == seed_user.refresh_token
 
 
 @pytest.mark.asyncio
-async def test_login_001_no_username_provided(monkeypatch, client, test_db, seed_user):
+async def test_login_001_no_username_provided(
+    monkeypatch: pytest.MonkeyPatch,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+) -> None:
     """
     Test 001 - Anomalous
     Conditions: Password provided, but username missing
     Result: HTTP 400 - {Pydantic error}
     """
     # Make the request
-    response = await client.post("/api/v1/users/login", json={"password": "password"})
+    response: Response = await client.post(
+        "/api/v1/users/login", json={"password": "password"}
+    )
     assert response.status_code == 422
-    response_data = response.json()
+    response_data: Any = response.json()
     assert response_data == {
         "detail": [
             {
@@ -81,27 +92,32 @@ async def test_login_001_no_username_provided(monkeypatch, client, test_db, seed
     assert seed_user.refresh_token is None
 
     # Verify that the user was not logged in
-    user_response = await test_db.execute(
+    user_response: Result[Tuple[User]] = await test_db.execute(
         select(User).filter_by(username=seed_user.username)
     )
-    user = user_response.scalars().first()
+    user: User | None = user_response.scalars().first()
     assert user is not None
     assert user.refresh_token is None
 
 
 @pytest.mark.asyncio
 async def test_login_002_anomalous_no_password_provided(
-    monkeypatch, client, test_db, seed_user
-):
+    monkeypatch: pytest.MonkeyPatch,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+) -> None:
     """
     Test 002 - Anomalous
     Conditions: Username provided, but password missing
     Result: HTTP 400 - {Pydantic error}
     """
     # Make the request
-    response = await client.post("/api/v1/users/login", json={"username": "test-user"})
+    response: Response = await client.post(
+        "/api/v1/users/login", json={"username": "test-user"}
+    )
     assert response.status_code == 422
-    response_data = response.json()
+    response_data: Any = response.json()
     assert response_data == {
         "detail": [
             {
@@ -116,63 +132,69 @@ async def test_login_002_anomalous_no_password_provided(
     assert seed_user.refresh_token is None
 
     # Verify that the user was not logged in
-    user_response = await test_db.execute(
+    user_response: Result[Tuple[User]] = await test_db.execute(
         select(User).filter_by(username=seed_user.username)
     )
-    user = user_response.scalars().first()
+    user: User | None = user_response.scalars().first()
     assert user is not None
     assert user.refresh_token is None
 
 
 @pytest.mark.asyncio
 async def test_login_003_anomalous_nonexistent_user(
-    monkeypatch, client, test_db, seed_user
-):
+    monkeypatch: pytest.MonkeyPatch,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+) -> None:
     """
     Test 003 - Anomalous
     Conditions: Username provided does not exist
     Result: HTTP 401 - "The provided credentials were incorrect"
     """
     # Make the request
-    response = await client.post(
+    response: Response = await client.post(
         "/api/v1/users/login", json={"username": "test-user2", "password": "password"}
     )
     assert response.status_code == 401
-    response_data = response.json()
+    response_data: Any = response.json()
     assert response_data == {"detail": "The provided credentials were incorrect"}
     assert seed_user.refresh_token is None
 
     # Verify that the user was not logged in
-    user_response = await test_db.execute(
+    user_response: Result[Tuple[User]] = await test_db.execute(
         select(User).filter_by(username=seed_user.username)
     )
-    user = user_response.scalars().first()
+    user: User | None = user_response.scalars().first()
     assert user is not None
     assert user.refresh_token is None
 
 
 @pytest.mark.asyncio
 async def test_login_004_anomalous_incorrect_password(
-    monkeypatch, client, test_db, seed_user
-):
+    monkeypatch: pytest.MonkeyPatch,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+) -> None:
     """
     Test 004 - Anomalous
     Conditions: Incorrect password provided
     Result: HTTP 401 - "The provided credentials were incorrect"
     """
     # Make the request
-    response = await client.post(
+    response: Response = await client.post(
         "/api/v1/users/login", json={"username": "test-user", "password": "password2"}
     )
     assert response.status_code == 401
-    response_data = response.json()
+    response_data: Any = response.json()
     assert response_data == {"detail": "The provided credentials were incorrect"}
     assert seed_user.refresh_token is None
 
     # Verify that the user was not logged in
-    user_response = await test_db.execute(
+    user_response: Result[Tuple[User]] = await test_db.execute(
         select(User).filter_by(username=seed_user.username)
     )
-    user = user_response.scalars().first()
+    user: User | None = user_response.scalars().first()
     assert user is not None
     assert user.refresh_token is None
