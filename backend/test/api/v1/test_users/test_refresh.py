5291e2f910facd2f20b2952bcb99617329e8a3fe
Ethan Brown <ethan@ewbrowntech.com>
2024-07-23 22:31:41 -0500

    Type hinting in test_refresh.py

--- backend/test/api/v1/test_users/test_refresh.py
+++ backend/test/api/v1/test_users/test_refresh.py
@@ -10,75 +10,89 @@ All rights reserved. This file is part of the Fourdrinier project and is release
 the GPLv3 License. See the LICENSE file for more details.
 """
 
-import secrets
 import pytest
 
-from fastapi.testclient import TestClient
+from httpx import AsyncClient, Response
+import secrets
+from typing import Any
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.schema import Column
 
 from backend.app.dependencies.core.auth.generate_jwt import generate_jwt
+from backend.app.db.models import User
 
 
 @pytest.mark.asyncio
 async def test_refresh_000_nominal(
-    client,
-    test_db,
-    seed_user,
-    test_jwt_secret_key,
-    test_jwt_expiration_time,
-):
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+    test_jwt_secret_key: str,
+    test_jwt_expiration_time: str,
+) -> None:
     """
     Test 000 - Nominal
     Conditions: Valid username and refresh token given
     Result: HTTP 201 - New JWT and refresh token returned
     """
     # Generate the JWT
-    jwt = generate_jwt(username=seed_user.username)
+    jwt: str = generate_jwt(username=str(seed_user.username))
 
     # Create a initial refresh token
-    initial_refresh_token = secrets.token_hex(32)
-    seed_user.refresh_token = initial_refresh_token
+    initial_refresh_token: str = secrets.token_hex(32)
+    seed_user.refresh_token = initial_refresh_token  # type: ignore
     await test_db.commit()
     await test_db.refresh(seed_user)
 
     # Refresh the JWT
-    query_params = {
+    query_params: dict[str, Column[str]] = {
         "refresh_token": seed_user.refresh_token,
         "client_id": seed_user.username,
     }
-    response = await client.post("/api/v1/users/refresh", params=query_params)
+    response: Response = await client.post(
+        "/api/v1/users/refresh", params=query_params  # type: ignore
+    )
     assert response.status_code == 200
-    response_data = response.json()
+    response_data: Any = response.json()
 
     # Ensure that the new refresh token is different from the initial token
     await test_db.refresh(seed_user)
-    assert seed_user.refresh_token != initial_refresh_token
+    assert str(seed_user.refresh_token) != initial_refresh_token
     assert seed_user.refresh_token == response_data["refresh_token"]
 
 
 @pytest.mark.asyncio
-async def test_refresh_001_anomalous_no_token(monkeypatch, client, test_db, seed_user):
+async def test_refresh_001_anomalous_no_token(
+    monkeypatch: pytest.MonkeyPatch,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+) -> None:
     """
     Test 001 - Anomalous
     Conditions: Valid username given, no refresh token given
     Result: HTTP 422 - Pydantic error
     """
     # Set the secret key
-    test_secret_key = secrets.token_hex(32)
+    test_secret_key: str = secrets.token_hex(32)
     monkeypatch.setenv("JWT_SECRET_KEY", test_secret_key)
 
     # Create a initial refresh token
-    initial_refresh_token = secrets.token_hex(32)
-    seed_user.refresh_token = initial_refresh_token
+    initial_refresh_token: str = secrets.token_hex(32)
+    seed_user.refresh_token = initial_refresh_token  # type: ignore
     await test_db.commit()
     await test_db.refresh(seed_user)
 
     # Make the request
-    query_params = {"client_id": seed_user.username}
-    response = await client.post("/api/v1/users/refresh", params=query_params)
+    query_params: dict[str, Column[str]] = {"client_id": seed_user.username}
+    response: Response = await client.post(
+        "/api/v1/users/refresh", params=query_params  # type: ignore
+    )
 
     # Ensure that the request failed
     assert response.status_code == 422
-    response_data = response.json()
+    response_data: Any = response.json()
     assert response_data["detail"] == [
         {
             "type": "missing",
@@ -91,35 +105,40 @@ async def test_refresh_001_anomalous_no_token(monkeypatch, client, test_db, seed
 
     # Ensure that the refresh token did not change
     await test_db.refresh(seed_user)
-    assert seed_user.refresh_token == initial_refresh_token
+    assert str(seed_user.refresh_token) == initial_refresh_token
 
 
 @pytest.mark.asyncio
 async def test_refresh_002_anomalous_no_username(
-    monkeypatch, client, test_db, seed_user
-):
+    monkeypatch: pytest.MonkeyPatch,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+) -> None:
     """
     Test 002 - Anomalous
     Conditions: No username given, valid refresh token given
     Result: HTTP 422 - Pydantic error
     """
     # Set the secret key
-    test_secret_key = secrets.token_hex(32)
+    test_secret_key: str = secrets.token_hex(32)
     monkeypatch.setenv("JWT_SECRET_KEY", test_secret_key)
 
     # Create a initial refresh token
-    initial_refresh_token = secrets.token_hex(32)
-    seed_user.refresh_token = initial_refresh_token
+    initial_refresh_token: str = secrets.token_hex(32)
+    seed_user.refresh_token = initial_refresh_token  # type: ignore
     await test_db.commit()
     await test_db.refresh(seed_user)
 
     # Make the request
-    query_params = {"refresh_token": seed_user.refresh_token}
-    response = await client.post("/api/v1/users/refresh", params=query_params)
+    query_params: dict[str, Column[str]] = {"refresh_token": seed_user.refresh_token}
+    response: Response = await client.post(
+        "/api/v1/users/refresh", params=query_params  # type: ignore
+    )
 
     # Ensure that the request failed
     assert response.status_code == 422
-    response_data = response.json()
+    response_data: Any = response.json()
     assert response_data["detail"] == [
         {
             "type": "missing",
@@ -132,106 +151,127 @@ async def test_refresh_002_anomalous_no_username(
 
     # Ensure that the refresh token did not change
     await test_db.refresh(seed_user)
-    assert seed_user.refresh_token == initial_refresh_token
+    assert str(seed_user.refresh_token) == initial_refresh_token
 
 
 @pytest.mark.asyncio
 async def test_refresh_003_anomalous_invalid_username(
-    monkeypatch, client, test_db, seed_user
-):
+    monkeypatch: pytest.MonkeyPatch,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+) -> None:
     """
     Test 003 - Anomalous
     Conditions: Invalid username given
     Result: HTTP 401 - "The provided credentials were incorrect"
     """
     # Set the secret key
-    test_secret_key = secrets.token_hex(32)
+    test_secret_key: str = secrets.token_hex(32)
     monkeypatch.setenv("JWT_SECRET_KEY", test_secret_key)
 
     # Create a initial refresh token
-    initial_refresh_token = secrets.token_hex(32)
-    seed_user.refresh_token = initial_refresh_token
+    initial_refresh_token: str = secrets.token_hex(32)
+    seed_user.refresh_token = initial_refresh_token  # type: ignore
     await test_db.commit()
     await test_db.refresh(seed_user)
 
     # Make the request
-    query_params = {
+    query_params: dict[str, str | Column[str]] = {
         "client_id": "invalid-user",
         "refresh_token": seed_user.refresh_token,
     }
-    response = await client.post("/api/v1/users/refresh", params=query_params)
+    response: Response = await client.post(
+        "/api/v1/users/refresh", params=query_params  # type: ignore
+    )
 
     # Ensure that the request failed
     assert response.status_code == 401
-    response_data = response.json()
+    response_data: Any = response.json()
     assert response_data["detail"] == "The provided credentials were incorrect"
 
     # Ensure that the refresh token did not change
     await test_db.refresh(seed_user)
-    assert seed_user.refresh_token == initial_refresh_token
+    assert str(seed_user.refresh_token) == initial_refresh_token
 
 
 @pytest.mark.asyncio
 async def test_refresh_004_anomalous_invalid_token(
-    monkeypatch, client, test_db, seed_user
-):
+    monkeypatch: pytest.MonkeyPatch,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+) -> None:
     """
     Test 004 - Anomalous
     Conditions: Invalid refresh token given
     Result: HTTP 401 - "The provided credentials were incorrect"
     """
     # Set the secret key
-    test_secret_key = secrets.token_hex(32)
+    test_secret_key: str = secrets.token_hex(32)
     monkeypatch.setenv("JWT_SECRET_KEY", test_secret_key)
 
     # Create a initial refresh token
-    initial_refresh_token = secrets.token_hex(32)
-    seed_user.refresh_token = initial_refresh_token
+    initial_refresh_token: str = secrets.token_hex(32)
+    seed_user.refresh_token = initial_refresh_token  # type: ignore
     await test_db.commit()
     await test_db.refresh(seed_user)
 
     # Make the request
-    query_params = {"client_id": seed_user.username, "refresh_token": "invalid-token"}
-    response = await client.post("/api/v1/users/refresh", params=query_params)
+    query_params: dict[str, Column[str] | str] = {
+        "client_id": seed_user.username,
+        "refresh_token": "invalid-token",
+    }
+    response: Response = await client.post(
+        "/api/v1/users/refresh", params=query_params  # type: ignore
+    )
 
     # Ensure that the request failed
     assert response.status_code == 401
-    response_data = response.json()
+    response_data: Any = response.json()
     assert response_data["detail"] == "The provided credentials were incorrect"
 
     # Ensure that the refresh token did not change
     await test_db.refresh(seed_user)
-    assert seed_user.refresh_token == initial_refresh_token
+    assert str(seed_user.refresh_token) == initial_refresh_token
 
 
 @pytest.mark.asyncio
 async def test_refresh_005_anomalous_invalid_username_and_token(
-    monkeypatch, client, test_db, seed_user
-):
+    monkeypatch: pytest.MonkeyPatch,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    seed_user: User,
+) -> None:
     """
     Test 005 - Anomalous
     Conditions: Invalid username and refresh token given
     Result: HTTP 401 - "The provided credentials were incorrect"
     """
     # Set the secret key
-    test_secret_key = secrets.token_hex(32)
+    test_secret_key: str = secrets.token_hex(32)
     monkeypatch.setenv("JWT_SECRET_KEY", test_secret_key)
 
     # Create a initial refresh token
-    initial_refresh_token = secrets.token_hex(32)
-    seed_user.refresh_token = initial_refresh_token
+    initial_refresh_token: str = secrets.token_hex(32)
+    seed_user.refresh_token = initial_refresh_token  # type: ignore
     await test_db.commit()
     await test_db.refresh(seed_user)
 
     # Make the request
-    query_params = {"client_id": "invalid-user", "refresh_token": "invalid-token"}
-    response = await client.post("/api/v1/users/refresh", params=query_params)
+    query_params: dict[str, str] = {
+        "client_id": "invalid-user",
+        "refresh_token": "invalid-token",
+    }
+    response: Response = await client.post(
+        "/api/v1/users/refresh", params=query_params  # type: ignore
+    )
 
     # Ensure that the request failed
     assert response.status_code == 401
-    response_data = response.json()
+    response_data: Any = response.json()
     assert response_data["detail"] == "The provided credentials were incorrect"
 
     # Ensure that the refresh token did not change
     await test_db.refresh(seed_user)
-    assert seed_user.refresh_token == initial_refresh_token
+    assert str(seed_user.refresh_token) == initial_refresh_token
