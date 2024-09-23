986ea591ecac24936a545596ca92f9a287062757
Ethan Brown <ethan@ewbrowntech.com>
2024-09-23 10:46:13 -0500

    Set up database session

--- backend/fourdrinier/db/session.py
+++ backend/fourdrinier/db/session.py
@@ -10,8 +10,31 @@ All rights reserved. This file is part of the Fourdrinier project and is release
 the GPLv3 License. See the LICENSE file for more details.
 """
 
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
 from sqlalchemy.orm import DeclarativeBase
 
+from backend.fourdrinier.core.config import ASYNC_DATABASE_URL
 
+
+# Create a base class for retrieval of model metadata
 class Base(DeclarativeBase):
     pass
+
+
+# Create an asynchronous database engine
+async_engine: AsyncEngine = create_async_engine(ASYNC_DATABASE_URL)
+AsyncSessionMaker = async_sessionmaker(bind=async_engine)
+
+
+# Return an asynchronous database session via dependency injection
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionMaker() as session:
+        try:
+            yield session
+        except Exception:
+            raise
