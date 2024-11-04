ed4b485f0d9c2685101d466f2fde8016575fd444
Ethan Brown <ethan@ewbrowntech.com>
2024-11-04 10:09:09 -0600

    Set up simple navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,16 +1,37 @@
 import { FC } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button"
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
 
 const Navbar: FC = () => {
     return (
-        <nav className="fixed top-0 left-0 right-0 w-full flex items-center justify-between p-4 bg-gray-800 text-white shadow-md z-50">
-            <div className="flex items-center">
-                <Link href="/" passHref>
-                    <Image src="/logo.png" alt="Logo" width={40} height={40} className="mr-2" />
-                </Link>
-            </div>
-        </nav >
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            <Image src="/logo.png" alt="Logo" width={40} height={40} />
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Servers
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
     );
 };
 
